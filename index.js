9f91d801a2e06ff9ed561e87c49df3933a88c506
Tommy <tooto1985@gamil.com>
2021-06-19 16:17:33 +0800

    modify

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 儲存 client 端的 sub 值，發送推撥通知的時候需要 sub 值才能發送給 client
 const fs = require('fs');
 const readFile = () => {
   var data = fs.readFileSync("./json/clientSubs.json").toString();
@@ -7,29 +8,42 @@ const writeFile = (data) => {
   fs.writeFileSync("./json/clientSubs.json", JSON.stringify(data, null, "\t"));
 }
 
-// ==================================================
-// 使用指令: web-push generate-vapid-keys 取得 private Key 和 public Key
+// 使用指令: web-push generate-vapid-keys 可以產生新的 private Key 和 public Key
 const webpush = require('web-push');
-const serverKey = {
+const vapidKeys = {
   publicKey: 'BPz4WOY59_ZR8exvjS08Y6ICcVOduUF8W48e6v9k0F2pUs7uxWgAyV8ntk6LB5EEF8iFPqTAYHPy5szLibGK1ic',
   privateKey: '_QJcl6YAW63hWnc_51HNk93_ea-mJ8XNFTCOEAgVquU',
 };
-webpush.setVapidDetails('mailto:tooto1985@gmail.com', serverKey.publicKey, serverKey.privateKey);
-// ==================================================
+webpush.setVapidDetails('mailto:tooto1985@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
+
+// 底下為 express 後端處理
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// 靜態檔案
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 取得 public key
+app.get('/publickey', (req, res) => { 
+  res.json(vapidKeys.publicKey);
+});
+
+// 儲存 sub
 app.post('/add', express.json(), (req, res) => {
   const clientSubs = readFile();
   clientSubs.push(req.body);
   writeFile(clientSubs);
   res.end();
 });
+
+// 取得 sub 清單
 app.get('/list', (req, res) => {
   const clientSubs = readFile();
   res.json(clientSubs);
 });
+
+// 發送推波(title=標題, body=內文, key=參數值)
 app.get('/push', (req, res) => {
   const clientSubs = readFile();
   clientSubs.forEach(clientSub => {
@@ -40,10 +54,11 @@ app.get('/push', (req, res) => {
         p256dh: clientSub.keys.p256dh,
       }
     };
-    webpush.sendNotification(pushConfig, JSON.stringify({ title: req.query.title || '新貼文', body: req.query.body || '有新增的貼文!!' })).catch(function(err) {
+    webpush.sendNotification(pushConfig, JSON.stringify({ title: req.query.title || '新貼文', body: req.query.body || '有新增的貼文!!', key: req.query.key })).catch(function(err) {
       console.log(err);
     });
   })
   res.end();
 });
+
 app.listen(process.env.PORT || 3000);
--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,5 @@
+let key = null;
+
 self.addEventListener('push', event => {
     console.log('[Service Worker] Push Received.');
     let title = 'Server Push';
@@ -7,6 +9,16 @@ self.addEventListener('push', event => {
     if (event.data) {
       options = event.data.json();
       title = options.title;
+      key = options.key;
     }
     event.waitUntil(self.registration.showNotification(title, options));
-})
+});
+
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(`http://localhost:3000/event.html?key=${key}`)
+  );
+});
